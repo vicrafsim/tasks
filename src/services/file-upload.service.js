85bfc2b278088eb93f56c45e550c415a16ca4869
Victor <victor.castanos@gmail.com>
2023-12-06 15:19:36 +0100

    ok

--- src/services/file-upload.service.js
+++ src/services/file-upload.service.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: "http://localhost:5005/"
+  baseURL: `${import.meta.env.VITE_SERVER_URL}`
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
 
