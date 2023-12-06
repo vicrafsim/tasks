e472f3940784f62fd59ea2655565b325e90356c0
Victor <victor.castanos@gmail.com>
2023-12-06 15:44:26 +0100

    attempt to merge in main

---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,104 @@
+import "./SignupPage.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import authService from "../../services/auth.service";
+
+function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleName = (e) => setName(e.target.value);
+  const handlePic = (e) => setPic(e.target.value);
+  const handleAge = (e) => {
+    setAge(e.target.value)
+  };
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    let role;
+    console.log(role);
+    Number(age) > 18 ? role = "Parent" : role = "Child";
+    // Create an object representing the request body
+    const requestBody = { email, password, name, role, age: Number(age) };
+    console.log(requestBody);
+    // Send a request to the server using axios
+    /* 
+    const authToken = localStorage.getItem("authToken");
+    axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/auth/signup`, 
+      requestBody, 
+      { headers: { Authorization: `Bearer ${authToken}` },
+    })
+    .then((response) => {})
+    */
+
+    // Or using a service
+    authService
+      .signup(requestBody)
+      .then((response) => {
+        // If the POST request is successful redirect to the login page
+        navigate("/login");
+      })
+      .catch((error) => {
+        // If the request resolves with an error, set the error message in the state
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className="signup-page">
+      <h1>Sign Up</h1>
+
+      <form className="form-container" onSubmit={handleSignupSubmit}>
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <label>Name:</label>
+        <input type="text" name="name" value={name} onChange={handleName} />
+
+        {/*         <label>Upload Photo:</label>
+        <input type='file' name="userPicture" value={name} onChange={handlePic} /> */}
+
+        <label>Age:</label>
+        <input type='number' name="age" value={age} onChange={handleAge} />
+
+
+        {/*          <label>Role:</label>
+        <select type='text' name="role" value={role} onChange={handleRole}>
+          {
+            (age < 18) ? <option value="Child">Child</option>
+            :
+            <option value="Parent">Parent</option>
+          }
+         
+          </select>  */}
+
+
+
+        <button type="submit" className="btn-navbar"><i class="fa-solid fa-door-open"></i></button>
+      </form>
+
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Already have account?</p>
+      <Link to={"/login"}> Login</Link>
+    </div>
+  );
+}
+
+export default SignupPage;
